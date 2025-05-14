8d1cc70b644931c08faeeeb87bcbedce10e3e3e6
Maciej Kaminski <maciej.kam03@gmail.com>
2025-05-14 21:30:20 +0200

    its emote you dumbass '~'

--- nvim/lua/my-config/packer.lua
+++ nvim/lua/my-config/packer.lua
@@ -22,4 +22,7 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('ThePrimeagen/harpoon')
 
+  use({'neovim/nvim-lspconfig'})
+  use({'hrsh7th/nvim-cmp'})
+  use({'hrsh7th/cmp-nvim-lsp'})
 end)
